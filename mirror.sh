790814b72d399c0ceb0b0f56c98fd9e68bd2064b
Lehner Florian <dev@der-flo.net>
2017-03-12 21:19:39 +0100

    [mirror] Use google hottrends instead of localhost
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- mirror.sh
+++ mirror.sh
@@ -3,7 +3,7 @@
 # get the display
 export DISPLAY=$(w -h $(whoami) | awk '$2 ~ /:[0-9.]*/{print $2}')
 # open firefox
-firefox 127.0.0.1:8080 &
+firefox https://trends.google.com/trends/hottrends/visualize &
 sleep 10
 # get firefox and hit F11
 WID=$(xdotool search --onlyvisible --name firefox | head -1)
